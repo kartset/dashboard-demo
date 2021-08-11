491d7c4246e5f23133b273f045f70a4022aa385c
Kartik Setia <kartset10@gmail.com>
2021-08-11 15:23:19 +0530

    In This Commit
    - Added a Badge in case of no topics

--- src/components/Topics.js
+++ src/components/Topics.js
@@ -1,4 +1,4 @@
-import { Button, HStack, Spacer, Heading, VStack, StackDivider, Text} from '@chakra-ui/react'
+import { Button, HStack, Spacer, Heading, VStack, StackDivider, Text, Badge} from '@chakra-ui/react'
 import React from 'react'
 import {useSelector} from 'react-redux'
 
@@ -10,6 +10,30 @@ function Topics({match}) {
     const dataState = useSelector((state) => state.data.dataState)
     console.log(dataState.find(e => e.class === cls).subjects.find(e => e.name === subName).chapters[chap-1].topics);
     const topics = dataState.find(e => e.class === cls).subjects.find(e => e.name === subName).chapters[chap-1].topics;
+
+    if(!topics.length) {
+      return (
+        <div>    
+        <VStack
+          divider={<StackDivider />} 
+          borderColor="gray.100" 
+          borderWidth="2px" p={4} 
+          borderRadius='lg'
+          alignItems='stretch'
+        >
+          <HStack>
+          <Heading>Topics</Heading>
+          <Spacer />
+          <Button>Add Topic</Button>
+          </HStack>
+          <Badge colorScheme="green" p="4" m="4" borderRadius='lg'>
+          No Topics added. Click add topics to add topics.
+          </Badge>
+          </VStack> 
+           
+        </div>
+      );
+    }
 
     return (
         <div>    
